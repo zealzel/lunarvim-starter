ea518e85fab381282494a6dc6dc6a27f327336ae
zealzel <zealzel@gmail.com>
2022-12-20 13:33:50 +0800

    modify keymappings

--- config.lua
+++ config.lua
@@ -13,24 +13,25 @@ vim.api.nvim_set_keymap('i', '<C-s>', '<esc>:w<cr>', {})
 
 
 lvim.keys.normal_mode["<C-g>"] = ":Git <CR>"
-lvim.keys.normal_mode["<leader>gh"] = ":Git hist<CR>"
-lvim.keys.normal_mode["<leader>go"] = ":Git checkout<space>"
-lvim.keys.normal_mode["<leader>gc"] = ":Git commit<CR>"
-
-lvim.keys.normal_mode["<leader>gca"] = ":Git commit --amend<CR>"
-lvim.keys.normal_mode["<leader>gl"] = ":Git log<CR>"
-lvim.keys.normal_mode["<leader>gr"] = ":Gread<CR>"
-lvim.keys.normal_mode["<leader>gw"] = ":Gwrite<CR><CR>"
-lvim.keys.normal_mode["<leader>gv"] = ":Gvdiffsplit<CR>"
-lvim.keys.normal_mode["<leader>gvv"] = ":Gvdiffsplit!<CR>"
-lvim.keys.normal_mode["<leader>gb"] = ":Git branch<Space>"
-lvim.keys.normal_mode["<leader>gm"] = ":Git merge<Space>"
-lvim.keys.normal_mode["<leader>gma"] = ":Git merge --abort<CR>"
-lvim.keys.normal_mode["<leader>grb"] = ":Git rebase<Space>"
-lvim.keys.normal_mode["<leader>grbc"] = ":Git rebase --continue<CR>"
-lvim.keys.normal_mode["<leader>grba"] = ":Git rebase --abort<CR>"
+-- lvim.keys.normal_mode["<leader>gh"] = ":Git hist<CR>"
+-- lvim.keys.normal_mode["<leader>go"] = ":Git checkout<space>"
+-- lvim.keys.normal_mode["<leader>gc"] = ":Git commit<CR>"
+-- lvim.keys.normal_mode["<leader>gca"] = ":Git commit --amend<CR>"
+-- lvim.keys.normal_mode["<leader>gl"] = ":Git log<CR>"
+-- lvim.keys.normal_mode["<leader>gr"] = ":Gread<CR>"
+-- lvim.keys.normal_mode["<leader>gw"] = ":Gwrite<CR><CR>"
+-- lvim.keys.normal_mode["<leader>gv"] = ":Gvdiffsplit<CR>"
+-- lvim.keys.normal_mode["<leader>gvv"] = ":Gvdiffsplit!<CR>"
+-- lvim.keys.normal_mode["<leader>gb"] = ":Git branch<Space>"
+-- lvim.keys.normal_mode["<leader>gm"] = ":Git merge<Space>"
+-- lvim.keys.normal_mode["<leader>gma"] = ":Git merge --abort<CR>"
+-- lvim.keys.normal_mode["<leader>grb"] = ":Git rebase<Space>"
+-- lvim.keys.normal_mode["<leader>grbc"] = ":Git rebase --continue<CR>"
+-- lvim.keys.normal_mode["<leader>grba"] = ":Git rebase --abort<CR>"
 
 lvim.keys.normal_mode["<leader>gp"] = ":!gitup<CR>"
+
+lvim.keys.normal_mode["<C-n>"] = ":NvimTreeToggle<cr>"
 
 vim.g.python3_host_prog = "~/.pyenv/versions/3.10.8/bin/python3.10"
 
